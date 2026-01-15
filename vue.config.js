4217159d86b0d04fad69185ad1aa4a860d147a6a
欧阳梓鸿 <1255050782@qq.com>
2026-01-15 22:02:44 +0800

    fix: 修复vue.config.js打包配置

--- vue.config.js
+++ vue.config.js
@@ -2,32 +2,19 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false, // 关闭 ESLint 保存检查
-
-  // --- 部署相关配置 ---
-  publicPath: '/',
+  lintOnSave: false,
+  productionSourceMap: false,
+  publicPath: '/', 
   outputDir: 'dist',
   assetsDir: 'assets',
-  productionSourceMap: false, // 生产环境不生成 SourceMap
 
-  // --- 修复点：使用 chainWebpack 去除 console.log ---
+  // 修复核心：使用 chainWebpack 替代 configureWebpack
   chainWebpack: config => {
-    // 只有在生产环境 (build) 时才执行
     if (process.env.NODE_ENV === 'production') {
       config.optimization.minimizer('terser').tap(args => {
-        // args[0] 是 terser 的配置对象
-        // 确保 compress 对象存在，防止报错
-        if (!args[0].terserOptions) {
-            args[0].terserOptions = {};
-        }
-        if (!args[0].terserOptions.compress) {
-            args[0].terserOptions.compress = {};
-        }
-        
-        // 开启去除 console 和 debugger
+        if (!args[0].terserOptions) args[0].terserOptions = {};
+        if (!args[0].terserOptions.compress) args[0].terserOptions.compress = {};
         args[0].terserOptions.compress.drop_console = true;
-        args[0].terserOptions.compress.drop_debugger = true;
-        
         return args;
       });
     }
